645fbc06695eb7d4018db8f5e17d0286a1adaa07
tylerdamonwilliams <williams13@grinnell.edu>
2019-04-24 14:37:29 -0500

    Update test.c

--- test.c
+++ test.c
@@ -3,36 +3,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h>  
+#include <stdlib.h> 
+#include <errno.h>   
+#include <sys/wait.h>
 
+   /*
 int main () {
+
    char command[50];
 
-<<<<<<< HEAD
-   while(1){
-=======
-int main ()
-{
+   while(){
 
-  printf("test\n");
-   // init FMOD sound system
-   FSOUND_Init (44100, 32, 0);
->>>>>>> dfdf14e46b5fd6d72b56ecb2ec9acd10580710ec
-
-   strcpy( command, "afplay music_library/ice.mp3 &" );
+   strcpy( command, "afplay music_library/StillHere.mp3 &" );
    system(command);
 
    sleep(5);
 
-<<<<<<< HEAD
    strcpy( command, "killall afplay" );
    system(command);
-}   
+}   */
 
-   return(0);
-} 
-=======
-   // clean up
-   FSOUND_Sample_Free (handle);
-   FSOUND_Close();
+//https://www.usna.edu/Users/cs/aviv/classes/ic221/s16/lec/14/lec.html
+
+//https://stackoverflow.com/questions/27306764/capturing-exit-status-code-of-child-process/27307246#27307246
+
+int main(int argc, char *argv[]) {
+   char song[50];
+
+   printf("Enter song: ");
+   gets(song);
+
+   char *args[] = {song, NULL};
+
+   pid_t c_pid, pid;
+   int status;
+
+   c_pid = fork();
+
+   if (c_pid == 0) {
+      execvp( "./input", args);
+    //only get here if exec failed                                                                                                                                             
+      perror("execve failed");
+   } else if (c_pid > 0) {
+        if ( waitpid(c_pid, &status, 0) == -1 ) {
+        perror("waitpid failed");
+        return EXIT_FAILURE;
+    }
+
+    if ( WIFEXITED(status) ) {
+        const int es = WEXITSTATUS(status);
+        printf("exit status was %d\n", es);
+    }
+   }
+    else {
+   perror("fork failed");
+   _exit(1);
 }
->>>>>>> dfdf14e46b5fd6d72b56ecb2ec9acd10580710ec
+
+return 0;
+}
